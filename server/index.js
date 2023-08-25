6cecc91d793647f656d9346555443f9462d18d7c
Caitlin Donahue <caitlindonahue@Caitlins-MacBook-Air.local>
2023-08-24 21:03:28 -0400

    added graphql to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { graphqlHTTP } = require('express-graphql');
 const connectDB = require('./db');
 
 const app = express();
@@ -7,6 +7,19 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+// Middleware
+app.use (
+  '/graphql',
+   graphqlHTTP({
+  schema,
+  graphiql: true,
+  })
+);
+
+
 app.listen(4002, () => {
   console.log('Server is running on port 4002..');
 });
+
+
+
