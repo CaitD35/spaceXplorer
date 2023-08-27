250b4f27514ebfdbdefbf98bf34017f495f2a800
Caitlin Donahue <caitlindonahue@Caitlins-MacBook-Air.local>
2023-08-27 10:53:03 -0400

    added components to MissionLib and Upcoming Miss

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
 import { ApolloProvider } from '@apollo/client';
 import client from './apolloClient';
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/missions" exact component={MissionLibrary} />
+      <Route path="/mission/:missionId" component={MissionDetail} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
