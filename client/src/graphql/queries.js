439d400862b72262ddee6a4fe8e69c447e4d5793
MilkyRamen <127574535+MilkyRamen@users.noreply.github.com>
2023-08-31 18:51:54 -0400

    queries.js

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,46 +1,61 @@
 import { gql } from '@apollo/client';
 
 export const GET_MISSIONS = gql`
-  query GetMissions {
+  query searchMission {
     missions {
       id
-      name
       rocketType
-      launchDate
-      status
-
-    }
-  }
-`;
-
-  export const GET_MISSION_DETAIL = gql`
-  query GetMissionDetail($id: ID!) {
-    mission(id: $id) {
-      id
-      name
-      rocketType
+      payload
       launchDate
       launchLocation
       landingLocation
       status
-      images
-      videos
-      objective
-      crewDetails 
+      media
+      missionBrief
+      missionObjective
+      crewDetails
 
     }
   }
 `;
 
-export const GET_NEXT_MISSION = gql`
-  query GetNextMission {
-    nextMission {
-      id
-      name
-      launchDate
+//   export const GET_MISSION_DETAIL = gql`
+//   query GetMissionDetail($id: ID!) {
+//     mission(id: $id) {
+//       id
+//       rocketType
+//       payload
+//       launchDate
+//       launchLocation
+//       landingLocation
+//       status
+//       media
+//       missionBrief
+//       missionObjective
+//       crewDetails
 
-    }
+//     }
+//   }
+// `;
+
+// export const GET_NEXT_MISSION = gql`
+//   query GetNextMission {
+//     nextMission {
+//       id
+//       name
+//       launchDate
+
+//     }
+//   }
+// `;
+
+export const QUERY_ME = gql`
+query me {
+  me {
+    _id
+    username
+    email
+    favoriteMissions
   }
-`;
-
+}`
 
