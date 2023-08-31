cf1fff1942845a245c74572e698303ddd6cbc989
Caitlin Donahue <caitlindonahue@Caitlins-MacBook-Air.local>
2023-08-31 13:58:37 -0400

    added some styling also fixed some of the structure

--- client/src/components/UpcomingMissions.jsx
+++ client/src/components/UpcomingMissions.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
-import Countdown from "./Countdown";
+// import { useQuery } from "@apollo/client";
+// import Countdown from "./Countdown";
 
 const UpcomingMissions = () => {
-  const { loading, error, data } = useQuery(/* query for upcoming missions */);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error}</p>;
-
   return (
     <div>
-      <Countdown />
+      {/* <Countdown /> */}
       <h1>Upcoming Missions</h1>
       {/* add code to display upcoming missions */}
     </div>
@@ -18,3 +13,20 @@ const UpcomingMissions = () => {
 };
 
 export default UpcomingMissions;
+
+// const UpcomingMissions = () => {
+//   const { loading, error, data } = useQuery(/* query for upcoming missions */);
+
+//   if (loading) return <p>Loading...</p>;
+//   if (error) return <p>Error : {error}</p>;
+
+//   return (
+//     <div>
+//       <Countdown />
+//       <h1>Upcoming Missions</h1>
+//       {/* add code to display upcoming missions */}
+//     </div>
+//   );
+// };
+
+// export default UpcomingMissions;
